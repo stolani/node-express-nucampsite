c1a7084ebb9662b8df152fd2b1fdbc5a007bb2ce
stolani <olansiki@gmail.com>
2021-04-11 16:27:25 -0400

    Workshop 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -30,18 +30,22 @@ campsiteRouter.route('/:campsiteId')
     next();
 })
     .get((req, res) => {
-        res.end('Will send all the campsites to you');
-    })
+        res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
     .post((req, res) => {
-        res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-    })
-    .put((req, res) => {
         res.statusCode = 403;
-        res.end('PUT operation not supported on /campsites');
-    })
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+
+    .put((req, res) => {
+        res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+        res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
     .delete((req, res) => {
-        res.end('Deleting all campsites');
+        res.end(`Deleting campsite: ${req.params.campsiteId}`);
     });
+    
 
 
 
